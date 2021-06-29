73374f2d007c90b92383e9740f34b22143a985ec
tkiran-mdsol <tkiran@mdsol.com>
2021-06-29 20:23:27 +0530

    updating logos

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import styles from './index.module.css';
+import './index.module.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
